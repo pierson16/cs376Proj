a8a5979bab2f28ca820950203d5156aff4a981ce
Emily Nitzberg <nitzberg16@up.edu>
2014-02-25 17:08:39 -0800

    main.c

--- main.c
+++ main.c
@@ -5,8 +5,10 @@
 #include "readLine.h"
 #include "execCmds.h"
 
+//PERSON 4: Emily Nitzberg
 // print the elements in the array, up to (but not including) the first
 // NULL entry
+
 void printLines(char** a) {
   int i;
   for (i = 0; a[i] != NULL; i++) {
@@ -14,11 +16,57 @@ void printLines(char** a) {
   }
 }
 
+void encrypt(char**a);
+void decrypt(char**a);
 // our array that tells how command-strings map to functions
 commandMap map[] = {
+  {"-e", encrypt},
+  {"-d", decrypt},
   {"-p", printLines},
   {NULL, NULL},
 };
+
+void encrypt(char**a)
+{
+  int j;
+  int i;
+  for(i = 0; a[i] != NULL; i++)
+    {
+      for(j = 0; a[i][j] != '\0'; j++)
+	{
+	  if(a[i][j] > 32)
+	    {
+	      a[i][j] += 1;
+	      if(a[i][j] == 127)
+		{
+		  a[i][j] = 33;
+		}
+	    }
+	}
+      
+    }
+}
+
+void decrypt(char**a)
+{
+  int j;
+  int i;
+  for(i = 0; a[i] != NULL; i++)
+    {
+      for(j = 0; a[i][j] != '\0'; j++)
+	{
+	  if(a[i][j] > 33)
+	    {
+	      a[i][j] -= 1;
+	      if(a[i][j] == 32)
+		{
+		  a[i][j] = 126;
+		}
+	    }
+	}
+      
+    }
+}
 
 // main function
 int main(int argc, char* argv[]) {
